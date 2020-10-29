e2c8582acdf3f4721934184d8f5bb0aadb41e38f
Talha Imran <talhaimran284@gmail.com>
2020-10-29 19:32:16 +0500

    Deleted few files and included bootstrap

--- src/App.js
+++ src/App.js
@@ -1,44 +1,13 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
 
-// Let's import bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-// Requiring in react-router-dom
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-// Requiring in components
-import Header from './components/layouts/Header';
-import Contacts from './components/contacts/Contacts';
-import AddContact from './components/contacts/addContact';
-import AboutPage from './components/pages/AboutPage';
-import NotFound from './components/pages/NotFound';
-
-// Requiring in privider
-import {Provider} from './Context';
-
-function App() {
-  return (
-
-    <Provider>
-      <div className="App">
-
-        <Router>
-          <Header/>
-          <div className="container">
-
-            <Switch>
-
-              <Route exact path="/" component={Contacts}></Route>
-              <Route exact path="/add-contact" component={AddContact}></Route>
-
-              <Route exact path="/about-page" component={AboutPage}></Route>
-              <Route component={NotFound}></Route>
-            </Switch>
-          </div>
-        </Router>
-    </div>
-    </Provider>
-  );
+export class app extends Component {
+    render() {
+        return (
+            <div className = "app">
+                <h1>Client Bar</h1>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default app;
