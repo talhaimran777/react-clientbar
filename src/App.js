f058d315b297ca84ce7b17d816a27afb9aa89452
Talha Imran <talhaimran284@gmail.com>
2020-10-29 20:03:33 +0500

    Rendering dashboard component on / get request

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Switch, } from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 
 // Components
 import Nav from './components/layouts/nav/navbar';
+import Dashboard from './components/dashboard/dashboard';
+
 export class app extends Component {
     render() {
         return (
-            <div className = "app">
-                <Router>
+            // Let's surround the app with the Router component 
+            // <div className = "app">
+            //     <Router>
+            //         <Nav/>
+            //     </Router>
+            // </div>
+
+
+            <Router>
+                <div className="app">
                     <Nav/>
-                </Router>
-            </div>
+                </div>
+
+                <Switch>
+                    <Route to exact = "/" component = {Dashboard}/>
+                </Switch>
+            </Router>
         )
     }
 }
---
+++ src/components/dashboard/dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class dashboard extends Component {
+    render() {
+        return (
+            <div className = "dashboard">
+                <h1>Dashboard Component</h1>
+            </div>
+        )
+    }
+}
+
+export default dashboard
