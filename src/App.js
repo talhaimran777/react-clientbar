3ae4898aa2364e3c91087346f3c16c790561b687
Talha Imran <talhaimran284@gmail.com>
2020-10-29 19:57:34 +0500

    Basic Navbar for the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Switch, } from 'react-router-dom';
 
+
+// Components
+import Nav from './components/layouts/nav/navbar';
 export class app extends Component {
     render() {
         return (
             <div className = "app">
-                <h1>Client Bar</h1>
+                <Router>
+                    <Nav/>
+                </Router>
             </div>
         )
     }
---
+++ src/components/layouts/nav/navbar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+export class navbar extends Component {
+    render() {
+        return (
+            <nav className = "navbar navbar-expand-md navbar-dark bg-primary p-0">
+                <div className="container px-5">
+
+                    <Link to = "/" className = "navbar-brand">Client Bar</Link>
+
+                    <button className="navbar-toggler"
+                            type = "button"
+                            data-toggle = "collapse"
+                            data-target = "#thisNav"
+                    >
+
+                        <span className="navbar-toggler-icon"
+                                style = {{fontSize: '15px'}}
+                        ></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="thisNav">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <Link className = "nav-link">Dashboard</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default navbar;
